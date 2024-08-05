b1c1ef81a900f36348d50618026c492114fea68e
yihang <yihangzzzzz@gmail.com>
2024-08-05 16:06:54 +0800

    init

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("tytyt")
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('system'));
+
+app.listen(3000, () => {
+    console.log("it works yay")
+})
